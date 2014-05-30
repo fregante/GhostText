a14493392e5288fd5ae91b5222064fe975f951ab
Federico Brigante <bfred-it@users.noreply.github.com>
2014-05-30 19:16:16 +0200

    Added some connection error handling

--- ChromeExtension/scripts/lib.js
+++ ChromeExtension/scripts/lib.js
@@ -26,16 +26,19 @@ var SublimeTextArea = {
         try {
             var webSocket = new WebSocket('ws://localhost:' + SublimeTextArea.serverPort());    
         } catch (e) {
-            if(e.name && e.name === "SecurityError") {
-                if(confirm('SublimeTextArea doesn\'t work on HTTPS pages in some versions of Chrome. Click OK to see how you can fix this.')){
-                    window.open('https://github.com/Cacodaimon/SublimeTextArea/issues/5#issuecomment-44571987')
-                }
-            }
+            that.errorHandler(e);
             return;
         }
 
         webSocket.onopen = function () {
             webSocket.send(that.textChange(title, textarea));
+        };
+        
+        webSocket.onerror = function (event) {
+            if (that.errorHandler(event)) {
+	            textarea.off('.sta');
+	            console.error('SublimeTexArea: detached from TextArea');
+            }
         };
 
         webSocket.onmessage = function (event) {
@@ -47,11 +50,10 @@ var SublimeTextArea = {
             textareaDom.focus();
         };
 
-        textarea.bind('input propertychange onmouseup', function() {
+        textarea.on('input.sta propertychange.sta onmouseup.sta', function() {
             webSocket.send(that.textChange(title, textarea));
         });
     },
-
     textChange: function (title, textarea) {
         var textareaDom = $(this).get(0);
 
@@ -63,5 +65,16 @@ var SublimeTextArea = {
                     end: textareaDom.selectionEnd
                 },
             });
+    },
+    errorHandler: function (e) {
+        if(e && e.target && e.target.readyState === 3) {
+            alert('Connection error. Make sure that Sublime Text is open and has SublimeTextArea installed. Try closing and opening it and try again. See if there are any errors in Sublime Text\'s console');   
+            return true;//the error was handled
+        } else if(e.name && e.name === "SecurityError") {
+            if(confirm('SublimeTextArea doesn\'t work on HTTPS pages in some versions of Chrome. Click OK to see how you can fix this.')){
+                window.open('https://github.com/Cacodaimon/SublimeTextArea/issues/5#issuecomment-44571987');
+            }
+            return true;//the error was handled
+        }
     },
 };
