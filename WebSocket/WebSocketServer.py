1e235d492ccca37881a7da75329307e871b00e79
Guido Kroemer <mail@cacodaemon.de>
2014-08-15 21:23:18 +0200

    WebSocker error 1006 on Windows systems #36

--- WebSocket/WebSocketServer.py
+++ WebSocket/WebSocketServer.py
@@ -3,35 +3,36 @@ import traceback
 from math import ceil
 from .Frame import Frame
 from .Handshake import Handshake
-
-
+from time import sleep
+ 
+ 
 class WebSocketServer:
     """
     A simple, single threaded, web socket server.
     """
-
+ 
     _id = 1
-
+ 
     def __init__(self, host='localhost', port=0):
         self._handshake = Handshake()
         self._frame = Frame()
-
+ 
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self._socket.bind((host, port))
-
+ 
         self._on_message_handler = None
         self._on_close_handler = None
         self._running = False
         self._conn = None
         self._address = None
         self._port = 0
-
+ 
         self._received_payload = ''
         self._id = WebSocketServer._id
         WebSocketServer._id += 1
         print("WebSocketServer id: {}".format(self._id))
-
+ 
     def start(self):
         """
         Starts the server,
@@ -42,20 +43,20 @@ class WebSocketServer:
         print('Listening on: {}'.format(self._port))
         self._running = True
         self._conn, self._address = self._socket.accept()
-
+ 
         data = self._conn.recv(1024)
         self._conn.sendall(self._handshake.perform(data).encode("utf-8"))
-
+ 
         while self._running:
             try:
                 header = self._conn.recv(24)  # Max web socket header length
             except OSError:
                 self._running = False
                 continue
-
+ 
             if len(data) > 0:
                 self._frame = Frame()
-
+ 
                 try:
                     self._frame.parse(header)
                 except IndexError:
@@ -63,15 +64,15 @@ class WebSocketServer:
                     print(traceback.format_exc())
                     self._running = False
                     continue
-
+ 
                 if self._frame.terminate:
                     self._running = False
                     continue
-
+ 
                 data = bytearray()
                 data.extend(header)
                 offset = self._frame.get_payload_offset()
-
+ 
                 if offset > 0:
                     try:
                         for offset_part in range(0, ceil(offset/4096)):
@@ -80,49 +81,54 @@ class WebSocketServer:
                         print(str(e))
                         print(traceback.format_exc())
                         continue
-
+ 
                 if self._frame.utf8:
                     try:
                         request = self._frame.get_payload(data).decode("utf-8")
                         self._received_payload += request.lstrip('\x00')
                     except UnicodeDecodeError:
                         continue
-
+ 
                 if self._frame.utf8 and self._frame.fin:
                     self._on_message_handler.on_message(self._received_payload)
                     self._received_payload = ''
-
+ 
         print('Stop')
         self.stop()
-
+ 
     def send_message(self, txt):
         """
         Sends a message if the server is in running state.
         """
         if not self._running:
             return
-
+ 
         self._frame = Frame()
         raw_data = self._frame.create(txt)
         self._conn.send(raw_data)
-
+ 
     def stop(self):
         """
         Stops the server by sending the fin package to the client and closing the socket.
         """
+ 
+        if not self._running:
+            return
+ 
         self._running = False
         try:
             self._conn.send(self._frame.close())
+            sleep(0.1)
             self._conn.close()
         except BrokenPipeError:
             print('Ignored BrokenPipeError')
         except OSError:
             print('Ignored OSError')
-
+ 
         if self._on_close_handler:
             print('Triggering on_close')
             self._on_close_handler.on_close()
-
+ 
     def on_message(self, handler):
         """
         Sets the on message handler.
@@ -130,7 +136,7 @@ class WebSocketServer:
         print('Setting on message handler')
         self._on_message_handler = handler
         self._on_message_handler.set_web_socket_server(self)
-
+ 
     def on_close(self, handler):
         """
         Sets the on connection closed handler.
@@ -138,19 +144,19 @@ class WebSocketServer:
         print('Setting on close handler')
         self._on_close_handler = handler
         self._on_close_handler.set_web_socket_server(self)
-
+ 
     def get_running(self):
         """
         Returns the running state.
         """
         return self._running
-
+ 
     def get_port(self):
         """
         Gets the port the server is listening/running on.
         """
         return self._port
-
+ 
     def get_id(self):
         """
         Gets the server's id.
