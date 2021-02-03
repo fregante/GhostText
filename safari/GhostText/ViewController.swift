bbdf17644ab3212789c13ef42727f485b71b80aa
Sindre Sorhus <sindresorhus@gmail.com>
2021-02-04 00:59:18 +0700

    Make version number work and cleanup (#189)

--- safari/GhostText/ViewController.swift
+++ safari/GhostText/ViewController.swift
@@ -5,30 +5,31 @@ import SafariServices.SFSafariExtensionManager
 let appName = "GhostText"
 let extensionBundleIdentifier = "com.fregante.GhostText.Extension"
 
-class ViewController: NSViewController {
-
-    @IBOutlet var appNameLabel: NSTextField!
+final class ViewController: NSViewController {
+    @IBOutlet private var appNameLabel: NSTextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.appNameLabel.stringValue = appName
+
+        appNameLabel.stringValue = appName
+
         SFSafariExtensionManager.getStateOfSafariExtension(withIdentifier: extensionBundleIdentifier) { (state, error) in
             guard let state = state, error == nil else {
                 // Insert code to inform the user that something went wrong.
                 return
             }
 
-            DispatchQueue.main.async {
+            DispatchQueue.main.async { [self] in
                 if (state.isEnabled) {
-                    self.appNameLabel.stringValue = "\(appName)'s extension is currently on."
+                    appNameLabel.stringValue = "\(appName)'s extension is currently on."
                 } else {
-                    self.appNameLabel.stringValue = "\(appName)'s extension is currently off. You can turn it on in Safari Extensions preferences."
+                    appNameLabel.stringValue = "\(appName)'s extension is currently off. You can turn it on in Safari Extensions preferences."
                 }
             }
         }
     }
 
-    @IBAction func openSafariExtensionPreferences(_ sender: AnyObject?) {
+    @IBAction private func openSafariExtensionPreferences(_ sender: AnyObject?) {
         SFSafariApplication.showPreferencesForExtension(withIdentifier: extensionBundleIdentifier) { error in
             guard error == nil else {
                 // Insert code to inform the user that something went wrong.
@@ -40,5 +41,4 @@ class ViewController: NSViewController {
             }
         }
     }
-
 }
