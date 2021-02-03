ca6b0a275cd546a4443615e46ee1918747bc3c54
Federico Brigante <me@fregante.com>
2021-02-02 19:28:57 -0600

    Plain safari-web-extension-converter
    xcrun safari-web-extension-converter distribution --app-name 'GhostText' --bundle-identifier 'com.fregante.ghosttext'
    
    And then rename the folder to `safari`

---
+++ safari/GhostText/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  GhostText
+//
+//  Created by Rico on 2/2/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ notification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
