64a5a2511107fcb7172d2b658546b9d7b97e9917
Federico Brigante <me@fregante.com>
2023-03-24 13:14:20 +0800

    Add keyboard shortcut customization instructions

--- source/options.js
+++ source/options.js
@@ -1,3 +1,9 @@
 import optionsStorage from './options-storage.js';
 
 optionsStorage.syncForm(document.querySelector('form'));
+
+if (location.protocol.startsWith('moz-')) {
+	document.documentElement.classList.add('firefox');
+} else if (location.protocol.startsWith('chrome-')) {
+	document.documentElement.classList.add('chrome');
+}
